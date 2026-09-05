f2ca4858cd9971ffdbde61559a2ba1363e325821
sabujAlom <sabujalom18@gmail.com>
2026-09-05 17:51:29 +0600

    changed the port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 const  { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config()
-const port = 3000
+const port = process.env.PORT;
 
 
 
