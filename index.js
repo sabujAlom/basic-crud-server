ffa3d14863e67b9083fac17d443acd86e21fa61e
sabujAlom <sabujalom18@gmail.com>
2026-09-06 00:00:13 +0600

    installed cors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 const  { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config()
+const cors = require('cors')
+app.use(cors());
 const port = process.env.PORT;
 
 
